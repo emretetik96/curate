491b7949a22dd205f6dbf4d4ae31776308ff81e7
emretetik96 <et2406@columbia.edu>
2018-01-08 21:08:33 -0500

    Added basic API interaction for User Pages

--- src/AllUsers.js
+++ src/AllUsers.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
+import UserAPI from './api'
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 
 class AllUsers extends Component {
 	render() {
-		return <div>This is all users</div>;
+		return (
+			<div>
+				<h1>
+					This is all users.
+				</h1>
+				<ListGroup>
+					{
+						UserAPI.users.map( (u) => (
+							<ListGroupItem>
+								<Link to={`/users/${u.username}`}>
+									{u.username}
+								</Link>
+							</ListGroupItem>
+						))
+					}
+				</ListGroup>
+			</div>
+			);
 	}
 }
 
--- src/User.js
+++ src/User.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import UserAPI from './api';
 
 class User extends Component {
 	render() {
-		return <div>This is a user</div>;
+		const user = UserAPI.get(this.props.match.params.username);
+		return (
+			<div>
+				<h1>{user.name}</h1>
+				<p>{user.description}</p>
+			</div>
+		);
 	}
 }
 
